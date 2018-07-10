19ddeedf43e94b8c9c369bc8e5fd6405a69e273e
Dwi Wicaksono <dwiwicaksono@Ndiw-MacBook-Pro.local>
2018-07-11 00:19:05 +0700

    revisi tugas ke 5 palindrome

--- exercises-5.js
+++ exercises-5.js
@@ -1,6 +1,6 @@
 function palindrome(kata) {
   var print = '';
-  for (i = kata.length-1; i >= 0; i--){
+  for (var i = kata.length-1; i >= 0; i--){
     print += kata[i];
   }
   if (kata === print){
