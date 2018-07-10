47ddc56a039090fb6ac78a2a6eb8e4d52fcd6e89
Dwi Wicaksono <dwiwicaksono@Ndiw-MacBook-Pro.local>
2018-07-10 23:45:38 +0700

    tugas ke 4 tantangan array 3

---
+++ exercises-4.js
@@ -0,0 +1,34 @@
+
+var input = ["0001", "Roman Alamsyah ", "Bandar Lampung", "21/05/1989", "Membaca"];
+
+function dataHandling2(input){
+input.splice(1, 2, "Roman Alamsyah Elsharawy", "Provinsi Bandar Lampung");
+input.splice(4, 1, "Pria", "SMA Internasional Metro");
+console.log(input);
+var slice = input[3];
+var split = slice.split('/');
+var num = Number(split[1]);
+switch (num){
+  case 01 : {console.log('Januari'); break;}
+  case 02 : {console.log('Februari'); break;}
+  case 03 : {console.log('Maret'); break;}
+  case 04 : {console.log('April'); break;}
+  case 05 : {console.log('Mei'); break;}
+  case 06 : {console.log('Juni'); break;}
+  case 07 : {console.log('Juli'); break;}
+  case 08 : {console.log('Agustus'); break;}
+  case 09 : {console.log('September'); break;}
+  case 10 : {console.log('Oktober'); break;}
+  case 11 : {console.log('November'); break;}
+  case 12 : {console.log('Desember'); break;}
+  default : {console.log('error');}
+}
+var d = split.join('-');
+var c = split.sort(function(a, b){ return b-a});
+console.log(c);
+console.log(d);
+var b = input[1];
+console.log(b.slice(0,15));
+}
+
+dataHandling2(input);
