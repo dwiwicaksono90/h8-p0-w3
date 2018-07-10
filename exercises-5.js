33ce7d9e77b0a72a247022144426135729dc398a
Dwi Wicaksono <dwiwicaksono@Ndiw-MacBook-Pro.local>
2018-07-11 00:16:51 +0700

    tugas ke 5 palindrome

---
+++ exercises-5.js
@@ -0,0 +1,18 @@
+function palindrome(kata) {
+  var print = '';
+  for (i = kata.length-1; i >= 0; i--){
+    print += kata[i];
+  }
+  if (kata === print){
+    return true;
+  } else {
+    return false;
+  }
+}
+
+// TEST CASES
+console.log(palindrome('katak')); // true
+console.log(palindrome('blanket')); // false
+console.log(palindrome('civic')); // true
+console.log(palindrome('kasur rusak')); // true
+console.log(palindrome('mister')); // false
