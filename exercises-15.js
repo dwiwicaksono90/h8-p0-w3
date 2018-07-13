70d0724a8830c851b87a627b8140a96dd54ced36
Dwi Wicaksono <dwiwicaksono@Ndiw-MacBook-Pro.local>
2018-07-13 15:19:57 +0700

    tugas 15 week 3

---
+++ exercises-15.js
@@ -0,0 +1,22 @@
+function groupAnimals(animals) {
+  animalsSort = animals.sort()
+  var temp = []
+  var hasil = []
+
+  for (var i = 0; i < animalsSort.length; i++){
+    if (i !== animalsSort.length-1 && animalsSort[i][0] === animalsSort[i+1][0]){
+      temp.push(animalsSort[i])
+    } else{
+      temp.push(animalsSort[i])
+      hasil.push(temp)
+      temp = []
+    }
+  }
+return hasil
+}
+
+// TEST CASES
+console.log(groupAnimals(['cacing', 'ayam', 'kuda', 'anoa', 'kancil']));
+// [ ['ayam', 'anoa'], ['cacing'], ['kuda', 'kancil'] ]
+console.log(groupAnimals(['cacing', 'ayam', 'kuda', 'anoa', 'kancil', 'unta', 'cicak' ]));
+// [ ['ayam', 'anoa'], ['cacing', 'cicak'], ['kuda'], ['unta'] ]
