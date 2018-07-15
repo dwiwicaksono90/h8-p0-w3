ab01db7df9b1c792f8744851e679df547fa427d2
Dwi Wicaksono <dwiwicaksono@Ndiw-MacBook-Pro.local>
2018-07-16 01:41:29 +0700

    revisi tugas 15

--- exercises-15.js
+++ exercises-15.js
@@ -1,22 +1,24 @@
 function groupAnimals(animals) {
-  var animalsSort = animals.sort()
-  var temp = []
-  var hasil = []
+  var alfabet = 'abcdefghijklmnopqrstuvwxyz'
+  var alfabetSplit = alfabet.split('')
+  // console.log(alfabetSplit)
+  var result =[]
 
-  for (var i = 0; i < animalsSort.length; i++){
-    if (i !== animalsSort.length-1 && animalsSort[i][0] === animalsSort[i+1][0]){
-      temp.push(animalsSort[i])
-    } else{
-      temp.push(animalsSort[i])
-      hasil.push(temp)
-      temp = []
+  for (var i = 0; i < alfabet.length; i++){
+    var temp = []
+    for (var j = 0; j < animals.length; j++){
+      if (alfabetSplit[i] === animals[j][0])
+      temp.push(animals[j])
+    }
+    if (temp.length !== 0){
+      result.push(temp)
     }
   }
-return hasil
+  return result
 }
 
 // TEST CASES
 console.log(groupAnimals(['cacing', 'ayam', 'kuda', 'anoa', 'kancil']));
 // [ ['ayam', 'anoa'], ['cacing'], ['kuda', 'kancil'] ]
 console.log(groupAnimals(['cacing', 'ayam', 'kuda', 'anoa', 'kancil', 'unta', 'cicak' ]));
-// [ ['ayam', 'anoa'], ['cacing', 'cicak'], ['kuda'], ['unta'] ]
+// [ ['ayam', 'anoa'], ['cacing', 'cicak'], ['kuda', 'kancil'], ['unta'] ]
